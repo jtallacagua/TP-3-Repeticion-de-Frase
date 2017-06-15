67e71995e9e3665f16d06b7498af69d27d440f2c
jtallacagua <tallacaguajesicaet5@gmail.com>
2017-06-15 11:37:08 -0300

    Create Interativa Estructurada.cpp

---
+++ Interativa Estructurada.cpp
@@ -0,0 +1,23 @@
+/* Trabajo #3 — Repeticion de frase - Iterativa Estructurada
+ Jesica Patrica Tallacagua Calle
+ 2017/06/15
+*/ 
+
+
+#include <iostream>
+using namespace std;
+
+int main ()
+
+{
+
+    int i=0;
+
+    
+    while (i<10)
+    {    
+        cout<<"Programá Jesi! "<<endl;	
+	    i=i+1;	
+	}
+
+}
