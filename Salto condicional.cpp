e1dd650131f71bfecfafeef49fa527ec0f5787ee
jtallacagua <tallacaguajesicaet5@gmail.com>
2017-06-15 11:28:28 -0300

    Create  Salto condicional.cpp

---
+++ Salto condicional.cpp
@@ -0,0 +1,22 @@
+	/ * Trabajo # 3 - Repeticion de frase
+	 Jesica Patrica Tallacagua Calle
+	 06/14/2017
+	* / 
+	
+	
+	# Incluir  < iostream >
+	el uso de  espacio de nombres  std ;
+	
+	int  principal ()
+	
+	{
+	
+		int i = 0 ;
+		UN:
+		tribunal << " Programa JESI! " << endl;
+		i = i + 1 ;
+		si (i < 10 )
+			Goto A;	
+			
+	}
+
